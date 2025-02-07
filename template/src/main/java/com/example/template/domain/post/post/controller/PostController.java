4cf513833503a6de211f2eda03195c60d635f201
jxxngin <wwd0203@daum.net>
2025-02-07 18:21:04 +0900

    [ã[feat] post entity

--- template/src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ template/src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.template.domain.post.post.controller;
 
+import com.example.template.domain.post.post.entity.Post;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -9,11 +10,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
+
+    List<Post> posts = new ArrayList();
+
+    public PostController() {
+        Post p1 = Post.builder()
+                .id(1L)
+                .title("title1")
+                .content("title1")
+                .build();
+
+        Post p2 = Post.builder()
+                .id(2L)
+                .title("title2")
+                .content("title2")
+                .build();
+
+        Post p3 = Post.builder()
+                .id(3L)
+                .title("title3")
+                .content("title3")
+                .build();
+
+        posts.add(p1);
+        posts.add(p2);
+        posts.add(p3);
+    }
 
     @GetMapping("/write")
     @ResponseBody
@@ -64,4 +93,20 @@ public class PostController {
                 </form>
                """.formatted(errorMsg, title, content);
     }
+
+    @GetMapping
+    @ResponseBody
+    private String showList() {
+        return """
+                <div>글 목록</div>
+                
+                <ul>
+                    <li>글1</li>
+                    <li>글2</li>
+                    <li>글3</li>
+                </ul>
+                
+                """;
+    }
+
 }
